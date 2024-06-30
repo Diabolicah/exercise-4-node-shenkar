ccf38e1b69ea21b41000ebfb583b1a6d2f8b3894
Diabolica Elias <scripter.diabolica@gmail.com>
2024-07-01 00:06:52 +0300

    updated the response error

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ exports.userController = {
         }
 
         const connection = await dbConnection.createConnection();
-        
+
         try {
             const access_code = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
             const [rows] = await connection.execute(`INSERT INTO ${TABLE_NAME} (user_name, user_password, user_access_code) VALUES (?, ?, ?)`, [username, password, access_code]);
@@ -24,7 +24,7 @@ exports.userController = {
                 access_token: access_code
             });
         } catch (error) {
-            res.status(500).json({ error: 'Internal server error at register user =' });
+            res.status(500).json(error);
         } finally {
             await connection.end();
         }
--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('api/user', userRouter);
+app.use('/api/user', userRouter);
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Not found' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
