a200e0a5be98e9008ab36fd92bb5725607993de9
Diabolica Elias <scripter.diabolica@gmail.com>
2024-07-07 21:29:09 +0300

    Added check for if user already exists

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,12 +33,18 @@ exports.userController = {
         const connection = await dbConnection.createConnection();
 
         try {
-            const access_code = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+            // Check if user already exists
+            const [usernames] = await connection.execute(`SELECT * FROM ${TABLE_NAME} WHERE user_name = ?`, [username]);
+            if (usernames.length > 0) {
+                return res.status(400).json({ error: 'Username already exists' });
+            }
 
             const [users] = await connection.execute(`SELECT * FROM ${TABLE_NAME}`);
             if (users.length >= 5) {
                 return res.status(400).json({ error: 'Maximum number of users reached (5)' });
             }
+
+            const access_code = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
             const [rows] = await connection.execute(`INSERT INTO ${TABLE_NAME} (user_name, user_password, user_access_code) VALUES (?, ?, ?)`, [username, password, access_code]);
             if (rows.affectedRows === 0) {
